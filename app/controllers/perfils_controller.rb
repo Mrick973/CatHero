5468f5edb239e5b4f962c424301ed012c1d4604d
Hemrick Donatien <do.hemrick@gmail.com>
2018-07-30 19:29:01 -0300

    update controller perfile

---
+++ app/controllers/perfils_controller.rb
@@ -0,0 +1,18 @@
+class PerfilsController < ApplicationController
+
+  # def index
+  #   @perfils = User.all
+  # end
+
+  #   def create
+  #   @perfil = User.new(_params)
+  #   if @booking.save
+  #     redirect_to booking_path(@booking)
+  #   else
+  #     render :new
+  #   end
+  # end
+  #   def booking_params
+  #     params.require(:booking).permit(:status, :start_date, :end_date)
+  #   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'perfils/index'
+  get 'perfils/create'
+  get 'perfils/show'
   # get 'bookings/create'
   # get 'bookings/index'
   resources :bookings
   devise_for :users
   root to: 'pages#home'
+   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
