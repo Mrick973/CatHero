9995ead46f329b6ca455211cf05be42db0168de4
Hemrick Donatien <do.hemrick@gmail.com>
2018-07-31 16:40:59 -0300

    Controllers and Models done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,21 +1,31 @@
 class FlatsController < ApplicationController
+ before_action :set_user, only: [:new, :index, :create]
+
   def new
-    @flat = Flat.new
+    @flat = @user.flats.new
   end
 
   def index
     @flats = Flat.all
   end
 
-    def create
+  def create
     @flat = Flat.new(flat_params)
+    @flat.user = @user
     if @flat.save
-      redirect_to flats_path(@flats)
+       redirect_to flats_path(@flat.user)
     else
       render :new
     end
   end
+
  private
+
+  def set_user
+    # @user = User.find(params[:user_id])
+    @user = current_user
+  end
+
   def flat_params
     params.require(:flat).permit(:description, :address)
   end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,4 @@
 class Animal < ApplicationRecord
   belongs_to :user
+  has_many :owner_bookings, foreign_key: :animal_id, class_name: "Booking"
 end
