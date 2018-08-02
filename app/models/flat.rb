40f5a3e7f5a6eeaa9b45ce160820f818a2c3456b
pedro medeiros <pedrinhomedeiros@hotmail.com>
2018-08-02 18:02:15 -0300

    feito o destroy do flat

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,7 +2,7 @@ class Flat < ApplicationRecord
   belongs_to :user
   has_many :caregiver_bookings,
            foreign_key: :flat_id,
-           class_name: "Booking"
+           class_name: "Booking", dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   mount_uploader :photo, PhotoUploader
