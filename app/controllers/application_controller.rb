aa5ca3165e960b06ad34dd37c1143c2849e0d611
Hemrick Donatien <do.hemrick@gmail.com>
2018-08-02 13:48:18 -0300

    PerfilController done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,8 @@ class ApplicationController < ActionController::Base
    new_animal_path
   end
 
-  def after_sign_up_path_for(resource)
+  # def after_sign_up_path_for(resource)
 
-  end
+  # end
 
 end
