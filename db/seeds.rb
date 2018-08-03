91fc1714a748d82a7441bf03f07bd7d73bb6b162
andre bomfim <alfbomfim@gmail.com>
2018-08-03 14:26:02 -0300

    Fixed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,11 @@ Animal.delete_all
 User.delete_all
 
 
-maria = User.create!(email: 'abc@gmail.com', phone: '555-5555', name: 'Maria', password: 'password')
+maria = User.create!(email: 'maria@gmail.com', phone: '555-5555', name: 'Maria', password: 'password')
 pedro = User.create!(email: 'pedro@gmail.com', phone: '352-5555', name: 'Pedro', password: 'password')
-joao = User.create!(email: 'wsx@gmail.com', phone: '555-5556', name: 'João' , password: 'password')
-joana = User.create!(email: 'rfv@gmail.com', phone: '555-5558', name: 'Joana', password: 'password')
-lucia = User.create!(email: 'tgb@gmail.com', phone: '555-5559', name: 'Lucia', password: 'password')
+joao = User.create!(email: 'joao@gmail.com', phone: '555-5556', name: 'João' , password: 'password')
+joana = User.create!(email: 'joana@gmail.com', phone: '555-5558', name: 'Joana', password: 'password')
+lucia = User.create!(email: 'lucia@gmail.com', phone: '555-5559', name: 'Lucia', password: 'password')
 
 
 floquinho = Animal.create!(species: 'Persa', name: 'Floquinho', user: maria )
@@ -16,14 +16,13 @@ biruta = Animal.create!(species: 'Siamês', name: 'Biruta', user: joao )
 bela = Animal.create!(species: 'Ragdoll', name: 'Bela', user: joana)
 cherry = Animal.create!(species: 'Maine Coon', name: 'Cherry', user: lucia)
 
-f1 = Flat.create!(description:'Mussum Ipsum, cacilds vidis litro abertis.', address:'Rua Pedralva, 171, alto de pinheiro, São Paulo', user: maria, remote_photo_url: 'http://res.cloudinary.com/ddtxrl2us/image/upload/c_fill,h_100,w_170/v1533238811/wclcxfrkkrtromtqxs77.jpg')
-f2 = Flat.create!(description:'Si num tem leite então bota uma pinga aí cumpadi', address:'Rua Mourato Coelho, 1404, São Paulo', user: joao, remote_photo_url: "http://res.cloudinary.com/ddtxrl2us/image/upload/v1533237242/cat_hero/maldivas.jpg")
-f3 = Flat.create!(description:'Toma um mé que o mundo vai girarzis!', address:'rua tabapua, 542, itaim bibi, São Paulo', user: joana, remote_photo_url: "https://res.cloudinary.com/ddtxrl2us/image/upload/v1533237242/cat_hero/download_1.jpg")
-f4 = Flat.create!(description:'Diuretics paradis num copo é motivis de denguis.', address:'Rua Pedroso alvarenga, 420, itaim bibi, São Paulo', user: lucia, remote_photo_url: "https://res.cloudinary.com/ddtxrl2us/image/upload/v1533237242/cat_hero/download.jpg")
-f5 = Flat.create!(description:'Diuretics paradis nm copo é motivis de denguis.', address:'Av. Paulista, 1000, são paulo', user: maria, remote_photo_url: "https://res.cloudinary.com/ddtxrl2us/image/upload/v1533237242/cat_hero/beco.jpg")
+f1 = Flat.create!(description:'Meu apartamento tem 60 metros. Aposto que ele vai adorar ser nosso hóspede!', address:'Rua Pedralva, 171, alto de pinheiro, São Paulo', user: maria, remote_photo_url: 'http://res.cloudinary.com/ddtxrl2us/image/upload/v1533315342/quao3xxjaxcqbwy9dvic.jpg')
+f2 = Flat.create!(description:'Ter espaço externo para correr, tomar sol, brincar, acesso a todos os cômodos da casa, passeios, muito carinho e amor.', address:'Rua Mourato Coelho, 1404, São Paulo', user: joao, remote_photo_url: "http://res.cloudinary.com/ddtxrl2us/image/upload/v1533315428/eh5hostq7suyotsbiqeo.jpg")
+f3 = Flat.create!(description:'Moramos num apartamento pequeno, porém de cômodos espaçosos e os acessos são liberados a todos os ambientes! ', address:'rua tabapua, 542, itaim bibi, São Paulo', user: joana, remote_photo_url: "https://res.cloudinary.com/ddtxrl2us/image/upload/v1533315476/vrgxsue9js8tgbd5owwf.jpg")
+f4 = Flat.create!(description:'Ele/Ela teria dois amiguinhos para brincar! São super bem educados e ficariam super felizes de ter mais companhia :) ', address:'Rua Pedroso alvarenga, 420, itaim bibi, São Paulo', user: lucia, remote_photo_url: "https://res.cloudinary.com/ddtxrl2us/image/upload/v1533315545/jcps446hyhsvfuuo5z3m.jpg")
 
 
-Booking.create!(status: true, start_date: Date.new(2018,9,1), end_date: Date.new(2018,10,1), animal: floquinho, flat: f1)
-Booking.create!(status: true, start_date: Date.new(2018,9,2), end_date: Date.new(2018,9,25), animal: biruta, flat: f2)
-Booking.create!(status: true, start_date: Date.new(2018,9,3), end_date: Date.new(2018,11,1), animal: bela, flat: f3)
+Booking.create!(status: true, start_date: Date.new(2018,7,1), end_date: Date.new(2018,8,1), animal: floquinho, flat: f1)
+Booking.create!(status: true, start_date: Date.new(2018,7,2), end_date: Date.new(2018,8,25), animal: biruta, flat: f2)
+Booking.create!(status: true, start_date: Date.new(2018,7,3), end_date: Date.new(2018,8,1), animal: bela, flat: f3)
 
